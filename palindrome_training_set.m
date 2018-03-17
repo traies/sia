4caad1344350975dfb5c45b93de9f7fa79f34110
traies <traies@itba.edu.ar>
2018-03-17 20:07:49 -0300

    Multilayer Perceptron implementation, works for palindrome of size 10
    However, with palindromes of size 100 it failes sometimes. We should
    probably add an alpha parameter with momentum and adaptative eta

---
+++ palindrome_training_set.m
@@ -0,0 +1,16 @@
+function [T, S] = palindrome_training_set(dim, samples, p = 0.8)
+  T = ones(samples,dim);
+  S = ones(samples,1);
+  for x = 1:samples
+    if (rand() < p)
+      for i = 1:(dim)/2
+        v = (rand() > 0.5) * 2 - 1;
+        T(x,i+1) = v;
+        T(x,dim-i+1) = v;
+      endfor
+    else
+      T(x, :) = (rand(1, dim) > 0.5) * 2 - 1 ;
+    endif
+    S(x) = palindrome(dim, T(x,:))*2-1;
+  endfor
+endfunction
--- train.m
+++ train.m
@@ -1,28 +1,84 @@
-function W = train(T, S, h, H)
+# T training set (bias not included)
+# S expected output
+# h number of hidden layers
+# H hidden layer dimensions (bias not included)
+# W neural network weights
+function W = train(T, S, h, H, eta=0.001)
  W = {};
  T_size = size(T)(2);
+ samples = length(S);
+ #  Consider bias in first layer size
+ prev = T_size + 1;
  
- prev = T_size;
+ # Weights initialization (consider bias in each hidden layer)
  for i = 1:h
-   W(end+1) = rand(prev,H(i));
-   prev = H(i);
+   W{i} = rand(prev,H(i));
+   prev = H(i) + 1;
  endfor
+ #  Initialize last layer
+ W{h+1} = rand(prev, 1);
  
  exit = 0;
- while(!exit) 
-  for x = randperm(T_size)
+ iter = 0;
+ while 1
+  # Samples are permutated on each iteration
+  err = 0;
+  for x = 1:samples
     V = {};
-    V_1 = {}; #derivatives g'(h_i) 
-    V(1) = T(x, :);
+    # g'(h_i)
+    V_1 = {};  
+    # Add bias
+    V{1} = [-1 T(x, :)];
+    # Feed forward, add bias to each layer
     for i = 1:h
-      V(i+1) = tanh(V{i}*W{i});
-      V_1(i) = 1- V(i+1) .** 2;
+      tempV = tanh(V{i}*W{i});
+      V{i+1} = [-1 tempV];
+      V_1{i+1} = (1- tempV .** 2);
     endfor
+    # Feed forward last layer
+    tempV = tanh(V{h+1}*W{h+1});
+    V{h+2} = tempV;
+    V_1{h+2} = 1- tempV .** 2;
     Delta = {};
-    Delta(1) = 1 - (V{h-1} * W) .** 2
+    # Find last delta (list indexes are backwards)
+    err += (S(x) - V{h+2}) ** 2;
+    Delta{h+1} =  V_1{h+2} .* (S(x) - V{h+2});
+    # Backpropagation
+    for i = h:-1:1
+      Delta{i} = V_1{i+1} .* (Delta{i+1} * W{i+1}(2:end, :)');
+    endfor
+    
+    # Update Weights
+    for i = 1:h+1
+      W{i} += eta * V{i}' * Delta{i} ;
+    endfor
+    
   endfor
   
- endwhile
- 
+  # Check accuracy
+  count = 0;
+  for s = 1:samples
+    # Add bias
+    proy = [-1 T(s, :)];
+    # Feed forward, add bias to each layer
+    for i = 1:h
+      proy = [-1 tanh(proy*W{i})];
+    endfor
+    proy = tanh(proy*W{h+1});
+    # Check if proyection is OK. Add to counter.
+    if( (S(s) > 0 && proy > 0) || (S(s) < 0 && proy <= 0))
+      count = count + 1;
+    endif
+  endfor
+  printf("Iter %g, Success rate: %f\n", iter, count);
+  printf("Error: %f \n", err);
+  fflush(stdout);
+  # Check quadratic error.
+  if count == samples
+    break
+  endif
+  iter += 1;
+  endwhile
+  
  endfunction
  
