bcc124591f8efb9dd754fc952f85c8755246c054
masaques <msaques@itba.edu.ar>
2018-03-18 12:19:54 -0300

    fix index in palindrome training set

--- palindrome_training_set.m
+++ palindrome_training_set.m
@@ -1,11 +1,11 @@
-function [T, S] = palindrome_training_set(dim, samples, p = 0.8)
+function [T, S] = palindrome_training_set(dim, samples, p = 0.5)
   T = ones(samples,dim);
   S = ones(samples,1);
   for x = 1:samples
     if (rand() < p)
-      for i = 1:(dim)/2
+      for i = 1:(dim/2)
         v = (rand() > 0.5) * 2 - 1;
-        T(x,i+1) = v;
+        T(x,i) = v;
         T(x,dim-i+1) = v;
       endfor
     else
